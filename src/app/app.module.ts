98875f5c05d6cc27c1fd777770d21a4ee871d779
Jose Guilherme De Oliveira <jgoliveira@brq.com>
2018-02-01 16:18:01 -0200

    Criando os componentes para tela de Restaurantes e para os itens do restaurante.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
