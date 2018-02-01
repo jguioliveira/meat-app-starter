eb65997e6cad13d2307ce343347d18a5f028fc08
Jose Guilherme De Oliveira <jgoliveira@brq.com>
2018-02-01 17:13:24 -0200

    Criando e configurando injeção do serviço para buscar restaurantes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from 'app/about/about.component';
-import { RestaurantsComponent } from 'app/restaurants/restaurants.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { Restaurant } from './restaurant/restaurant.model';
+import { RestaurantsService } from './restaurants.service';
 
 @Component({
   selector: 'mt-restaurants',
@@ -7,36 +8,11 @@ import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
 })
 export class RestaurantsComponent implements OnInit {
 
-  constructor() { }
+  restaurants: Restaurant[];
 
-  restaurants: Restaurant[] = [
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png"
-    },
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png"
-    },
-    {
-      id: "coffee-corner",
-      name: "Coffee Corner",
-      category: "Coffee Shop",
-      deliveryEstimate: "30-40m",
-      rating: 4.8,
-      imagePath: "assets/img/restaurants/coffeecorner.png"
-    }
-  ];
+  constructor(private restaurantService: RestaurantsService) { }
 
   ngOnInit() {
+    this.restaurants = this.restaurantService.restaurants();
   }
-
 }
